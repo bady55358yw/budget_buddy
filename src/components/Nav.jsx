fefc44953147a60d92e37ab66aa53c40f9f45258
bady55358yw <bady55358yw@gmail.com>
2024-11-25 12:23:56 +0800

    change logo and favicon

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import { Link,Form } from 'react-router-dom'
 
 // 引入 icon
-import {ChartBarIcon,UserMinusIcon} from '@heroicons/react/24/outline'
+import {UserMinusIcon} from '@heroicons/react/24/outline'
 
 // --- 底下開始撰寫 ---
 
 export default function Nav(props) {
-    // console.log('@@--userName',props.userName);
     const {userName} = props
 
     function checkDeleteUser(e) {
@@ -19,7 +18,7 @@ export default function Nav(props) {
     return (
         <nav id="navbar" className='bg-primary flex justify-between px-6 py-2'>
             <Link to='/' className='text-lightAccent1 flex items-center gap-1 group'>
-                <ChartBarIcon width={32} />
+                <img src={`${process.env.PUBLIC_URL}/assets/logo.svg`} alt="logo" width={32} />
                 <span className='text-lg transition-all duration-400 group-hover:text-primaryLight2'>記帳小幫手</span>
             </Link>
             {
